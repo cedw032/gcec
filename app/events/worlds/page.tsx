7e4cd14a872f10417e9999fc7da03a4b99fab649
Chad Edwards <chad.edwards.mail@gmail.com>
2024-02-18 15:39:53 +1000

    add skeleton pages

---
+++ app/events/worlds/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      VEX World Championship 2023/2024
+    </main>
+  );
+}
